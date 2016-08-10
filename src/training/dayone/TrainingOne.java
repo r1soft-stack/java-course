893d94f7f72c0188bf1836b45954f7ed6c73e4e2
masettir <masettir@MOBTEK602.yoox.net>
2016-08-10 16:52:20 +0200

    first commit

---
+++ src/training/dayone/TrainingOne.java
@@ -0,0 +1,7 @@
+package training.dayone;
+
+/**
+ * Created by masettir on 10/08/2016.
+ */
+public class TrainingOne {
+}
---
+++ src/training/daytwo/TrainingTwo.java
@@ -0,0 +1,7 @@
+package training.daytwo;
+
+/**
+ * Created by masettir on 10/08/2016.
+ */
+public class TrainingTwo {
+}
